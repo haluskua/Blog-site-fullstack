645d627b6ec9f589f7cafca4b7b03260d9444a3f
haluskua <haluskua.one@gmail.com>
2021-08-21 11:14:45 +1200

    added image and begin styling for wrtie.jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import TopBar from "./components/topbar/TopBar";
 import Write from "./components/write/Write";
-import Single from "./pages/single/Single";
+// import Single from "./pages/single/Single";
 
 function App() {
   return (
--- src/components/write/Write.jsx
+++ src/components/write/Write.jsx
@@ -1,12 +1,14 @@
 import "./write.css";
+import writeImg from "../../images/profile.jpg";
 
 export default function Write() {
   return (
     <div className="write">
+      <img className="writeImg" src={writeImg} alt="" />
       <form action="" className="write">
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
-            <i class="fas fa-plus"></i>
+            <i className="writeIcon fas fa-plus"></i>
           </label>
           <input type="file" id="fileInput" style={{ display: "none" }} />
           <input
@@ -16,6 +18,18 @@ export default function Write() {
             autoFocus={true}
           />
         </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
       </form>
     </div>
   );
