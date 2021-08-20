e9e85e27e43a18c89d471de84c64cdc7513334d1
haluskua <haluskua.one@gmail.com>
2021-08-20 13:22:00 +1200

    added minor box-shadow to posts page

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -8,13 +8,32 @@ export default function Post() {
       <div className="postInfo">
         <div className="postCats">
           <div className="postCat">Music</div>
-          <div className="postCat">Music</div>
+          <div className="postCat">Life</div>
           <span className="postTitle">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-            perspiciatis at dignissimos nobis ad. Architecto?
           </span>
-          <hr />
+          <hr className="line-height" />
           <div className="postDate"> 1 hour ago</div>
+          <div className="postDescription">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, quas
+            enim qui, nam omnis inventore iste voluptates, sit error asperiores
+            eligendi voluptatum aut eius totam praesentium harum dolorem illum
+            numquam nulla laboriosam blanditiis deleniti? Dolor, iusto nam qui
+            autem dicta nisi molestiae dolores ipsum aspernatur optio asperiores
+            mollitia minus beatae quasi sapiente! Est delectus doloremque
+            laboriosam, voluptatem reiciendis architecto officiis libero
+            ratione, a inventore minus, quia quos! Dolore deleniti amet est
+            aspernatur, quos accusantium odit mollitia doloribus sunt tenetur
+            laborum, minima nemo quibusdam, tempora ex temporibus totam!
+            Quibusdam libero quis adipisci tempore magnam quia ut asperiores
+            accusamus voluptatibus labore! Quas nemo asperiores atque magni
+            exercitationem suscipit eum praesentium quaerat! Officiis iure
+            repellat dolore voluptatum, quam, debitis mollitia aliquid
+            exercitationem harum velit consequuntur. Nam optio natus a. Quasi
+            earum quos, maiores odio beatae cum, atque quod tenetur dolorem
+            molestias nisi repellat quia adipisci recusandae velit, porro
+            exercitationem architecto excepturi. Magnam, nihil.
+          </div>
         </div>
       </div>
     </div>
