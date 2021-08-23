01bd111110c47789c6a5253fd3ae8b5b6dff6d6c
haluskua <haluskua.one@gmail.com>
2021-08-23 14:32:37 +1200

    added express restructured folders into API and Client added MongoDB and dotenv files and routes with Postman integration

---
+++ api/index.js
@@ -0,0 +1,29 @@
+//creating express server
+
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("Connected to your MongoDB Database"))
+  .catch((err) => console.log(err));
+
+// app.use("/", (req, res) => {
+//   console.log("hey this is the main url");
+// });
+
+app.use("/api/auth", authRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running!");
+});
