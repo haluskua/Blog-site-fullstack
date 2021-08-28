5886efb204a75324f99b14f3518958e6f8deb3ce
haluskua <haluskua.one@gmail.com>
2021-08-29 01:47:51 +1200

    added axios and set up axios and data test on Postman through Router at Home.js

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,8 +2,19 @@ import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [post, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/api/posts");
+      console.log(res);
+    };
+    fetchPosts();
+  }, []);
   return (
     <>
       <Header />
