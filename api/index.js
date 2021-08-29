47142e77b2d59e221b12e12f3ebf075f78c016a3
haluskua <haluskua.one@gmail.com>
2021-08-30 06:52:16 +1200

    added proxy to package.json

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
+const port = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
@@ -45,6 +46,4 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
-  console.log("Backend is running!");
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
