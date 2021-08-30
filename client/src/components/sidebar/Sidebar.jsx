ed7eda45e64e1fd1816c37345077268bc7f01c3e
haluskua <haluskua.one@gmail.com>
2021-08-31 10:41:46 +1200

    added connection for side bar list items

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,25 @@
 import "./sidebar.css";
 import myPic from "../../images/profile.jpg";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/api/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebarItem">
         <span className="sidebarTitle">About Me</span>
         <img className="my-pic" src={myPic} alt="profile-pic" />
-        <p className="sidbarPgh">
+        <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
           necessitatibus a laborum atque ratione quasi commodi animi earu ipsam.
         </p>
@@ -15,11 +27,9 @@ export default function Sidebar() {
       <div className="sidebarItem">
         <span className="sidebarTitle">CATEGORIES</span>
         <ul className="sidebarList">
-          <li className="sidebarListItem">Life</li>
-          <li className="sidebarListItem">Music</li>
-          <li className="sidebarListItem">Style</li>
-          <li className="sidebarListItem">Tech</li>
-          <li className="sidebarListItem">Cinema</li>
+          {cats.map((c) => (
+            <li className="sidebarListItem">{c.name}</li>
+          ))}
         </ul>
       </div>
       <div className="sidebarItem">
