14ad91132c2398c74a6b257ae6ea17639ae3216c
haluskua <haluskua.one@gmail.com>
2021-09-15 00:12:09 +1200

    output input on register to console from API database

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,30 +1,61 @@
-import "./register.css";
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./register.css";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const res = await axios.post("/api/auth/register", {
+        username,
+        email,
+        password,
+      });
+      // res.data && window.location.replace("/api/auth/login");
+      console.log("registering");
+      console.log(res);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
   return (
     <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm">
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
         <input
           type="text"
           className="registerInput"
           placeholder="Enter your username..."
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label>Email</label>
         <input
           type="text"
           className="registerInput"
           placeholder="Enter email..."
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label>Password</label>
         <input
           type="text"
           className="registerInput"
           placeholder="Enter password..."
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="registerButton">Register</button>
+        <button className="registerButton" type="submit">
+          <Link className="link" to="/register">
+            Register
+          </Link>
+        </button>
         <button className="registerLoginButton">
           <Link className="link" to="/login">
             Login
